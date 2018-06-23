25d2b8588f82c4638e1669f10ed9063525e5a125
Leo Costa <you@example.com>
2018-06-23 00:39:04 -0300

    Change init to loader

--- src/main/java/br/com/lelo/vestibular/EmpresaLoader.java
+++ src/main/java/br/com/lelo/vestibular/EmpresaLoader.java
@@ -4,24 +4,24 @@ import br.com.lelo.vestibular.empresa.domain.Empresa;
 import br.com.lelo.vestibular.empresa.service.EmpresaCommandService;
 import br.com.lelo.vestibular.empresa.service.EmpresaQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
-public class EmpresaConfiguration {
+public class EmpresaLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     private EmpresaCommandService command;
     private EmpresaQueryService query;
 
     @Autowired
-    public EmpresaConfiguration(EmpresaCommandService command, EmpresaQueryService query) {
+    public EmpresaLoader(EmpresaCommandService command, EmpresaQueryService query) {
         this.command = command;
         this.query = query;
     }
 
-    @PostConstruct
-    public void initializer() {
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         if (query.empresasIsEmpty()) {
             command.save(new Empresa("Unesp - Universidade Estadual de SP"));
             command.save(new Empresa("Usp - Universidade de SP"));
